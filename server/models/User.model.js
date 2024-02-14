c8154078ebced9e8b7b1cf4145683822e7d27886
Tomas De Piano <tomasdepiano1@gmail.com>
2024-02-13 20:43:19 -0700

    Tomas; created the server folder and added db.js to config file; added the models inside the server file, still working on user.model.js

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,6 +1,6 @@
 import { Datatypes, Model } from "sequelize";
 import util from "util";
-import { db } from "../db";
+import { db } from "../config/db";
 
 export default class User extends Model {
     [util.inspect.custom]() {
