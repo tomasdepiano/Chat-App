fcf642ae6beaae270894dcd9674317987b71aa2a
Tomas De Piano <tomasdepiano1@gmail.com>
2024-02-13 20:36:42 -0700

    Tomas: created the db.js, index.js. user.model.js, chats.models.js, messages.model.js files; add the code for db.js and started on the user.model.js code, but not finished yet

---
+++ config/models/User.model.js
@@ -0,0 +1,21 @@
+import { Datatypes, Model } from "sequelize";
+import util from "util";
+import { db } from "../db";
+
+export default class User extends Model {
+    [util.inspect.custom]() {
+        return this.toJSON();
+    }
+}
+
+
+User.init({
+    userId: {
+        type: Datatypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    }, 
+    fname: {
+        type: Datatypes.
+    }
+})
