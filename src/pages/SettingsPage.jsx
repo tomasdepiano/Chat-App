04b00c700e5738102bc54297b2b0303d47bbefd4
Mounika Krishna <mounikaa.krishna@gmail.com>
2024-02-17 16:59:26 -0700

    Mounika:Worked on ChangeUserName Modal

---
+++ src/pages/SettingsPage.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+import ChangeUserName from '../modals/ChangeUserName';
+
+function SettingsPage() {
+  const [changeUserName, setChangeUserName] = useState(false);
+  const handleOnClose = () => setChangeUserName(false);
+  return (
+    <main className="h-screen w-screen bg-blue-400 ">
+      <h1 className="text-xl font-bold text-center p-2">Settings</h1>
+      <section className="flex flex-col items-center w-full mt-8 justify-evenly">
+        <article className="flex">
+          <lable className="p-2">UserName:</lable>
+          <input
+            type="text"
+            className="border-2 border-red-500 w-22 max-w-xs rounded-md "
+          />
+          <button
+            onClick={() => {
+              // e.preventDefault();
+              setChangeUserName(true);
+            }}
+            className="w-22 border-2 border-gray-400 rounded-lg bg-gray-100 ml-2 p-1.5 hover:bg-gray-400"
+          >
+            Change UserName
+          </button>
+        </article>
+
+        <article className="flex mt-4 justify-evenly">
+          <label className="p-2">PassWord:</label>
+          <input
+            type="password"
+            className=" w-22 border-2 border-red-500 max-w-xs rounded-md p-1"
+          />
+          <button className="w-22 border-2 border-gray-400 rounded-lg bg-gray-100 ml-2 p-1.5 hover:bg-gray-400">
+            Change Password
+          </button>
+        </article>
+        <article className="flex mt-4 justify-evenly">
+          <label className="p-2 ">Email:</label>
+          <input
+            type="email"
+            className="border-2 border-red-500 w-22 max-w-xs rounded-md"
+          />
+          <button className="w-22 border-2 border-gray-400 rounded-lg bg-gray-100 ml-2 pl-6 pr-6 hover:bg-gray-400">
+            Change Email
+          </button>
+        </article>
+      </section>
+      <ChangeUserName onClose={handleOnClose} visible={changeUserName} />
+    </main>
+  );
+}
+
+export default SettingsPage;
