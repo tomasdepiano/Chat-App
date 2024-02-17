7f8dbda686a74cb6cde2b1744941c8bcbcf297fd
Tomas De Piano <tomasdepiano1@gmail.com>
2024-02-17 15:38:28 -0700

    Finished the createaccount modal and installed axios; added the some code to the loginpage to render the modal when you click on create an account button

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import CreateAccountModal from "../modals/CreateAccountModal";
 
 export default function LoginPage() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOnClose = () => setShowModal(false);
   return (
     <main className="bg-blue-400 h-screen justify-center">
       <div className="flex flex-col items-center text-3xl font-bold underline">
@@ -12,14 +16,14 @@ export default function LoginPage() {
           <label className="p-2">Username:</label>
           <input
             placeholder="Type here"
-            className=" border-2 border-red-400 w-full rounded-md max-w-xs"
+            className=" border-2 border-red-400 w-full rounded-md max-w-xs p-2"
             type="text"
             name="email"
           />
           <label className="p-2">Password:</label>
           <input
             placeholder="Type here"
-            className="border-2  border-red-400 w-full rounded-md max-w-xs"
+            className="border-2  border-red-400 w-full rounded-md max-w-xs p-2"
             type="password"
             name="password"
           />
@@ -30,10 +34,14 @@ export default function LoginPage() {
             Sign In
           </button>
         </form>
-        <button className="border-2 border-red-400 rounded p-2 m-2">
+        <button
+          onClick={() => setShowModal(true)}
+          className="border-2 border-red-400 rounded p-2 m-2"
+        >
           Create An Account
         </button>
       </div>
+      <CreateAccountModal onClose={handleOnClose} visible={showModal} />
     </main>
   );
 }
