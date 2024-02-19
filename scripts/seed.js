2c1f718d1625bf73dfcf0f5dc84ff7291dbfedf2
Tomas De Piano <tomasdepiano1@gmail.com>
2024-02-19 13:19:22 -0700

    Added a data folder inside of scripts with a seed.json file that contains dummy data to seed DB; worked on seed.js and was able to seed the DB; added some more tailwind features on the login page

--- scripts/seed.js
+++ scripts/seed.js
@@ -2,9 +2,29 @@ import { db } from "../server/config/db.js";
 import User from "../server/models/User.model.js";
 import Chats from "../server/models/Chats.model.js";
 import Messages from "../server/models/Messages.models.js";
+import UserData from "./data/seed.json" assert { type: "json" };
 
 console.log("Syncing Database");
 
 await db.sync({ force: true });
 
 console.log("Seeding database...");
+
+const usersInDB = await Promise.all(
+  UserData.map(async (data) => {
+    const { userId, fname, lname, email, password, username } = data;
+
+    const newUser = await User.create({
+      userId: userId,
+      fname: fname,
+      lname: lname,
+      email: email,
+      password: password,
+      username: username,
+    });
+
+    return newUser;
+  })
+);
+
+console.log(usersInDB);
