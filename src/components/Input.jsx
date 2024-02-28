972f9a073c06b44f25dde1c7feeeee42559d0c93
Mounika Krishna <mounikaa.krishna@gmail.com>
2024-02-27 18:51:31 -0700

    removed the spread operator{...props} passed placeholder as prop value

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,28 +1,28 @@
-import React from "react";
+import React from 'react';
 
-export const Input = ({ label, value, onChange, ...props }) => {
+export const Input = ({ label, value, onChange, placeholder }) => {
   return (
     <div className="flex items-center w-96">
       <label className="p-2 text-white text-xl w-2/5">{label}</label>
       <input
-        {...props}
         className="border-2 border-red-400 w-full rounded-md max-w-xs p-2 "
         onChange={onChange}
         value={value}
+        placeholder={placeholder}
       />
     </div>
   );
 };
 
-export const ModalInputs = ({ label, value, onChange, ...props }) => {
+export const ModalInputs = ({ label, value, onChange, placeholder }) => {
   return (
     <div className="flex items-center w-96">
       <label className="p-2 text-white text-xl w-1/3">{label}</label>
       <input
-        {...props}
         className="ml-2 p-2 border-2 border-red-400 rounded"
         onChange={onChange}
         value={value}
+        placeholder={placeholder}
       />
     </div>
   );
