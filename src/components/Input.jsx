db7ec3eb7bb7d36517c054d60d124daa11bb9eaf
Tomas De Piano <tomasdepiano1@gmail.com>
2024-03-05 20:25:16 -0700

    Added extra tailwind responsive screen, also made some changes to login page based on the size of screen

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 export default function Input({
   label,
@@ -9,8 +9,14 @@ export default function Input({
   type,
 }) {
   return (
-    <div className={`flex items-center w-96 ${className}`}>
-      {label && <label className="p-2 text-white text-xl w-2/5">{label}</label>}
+    <div
+      className={`lg:flex lg:items-center lg:w-96 sm:flex sm:flex-none ${className}`}
+    >
+      {label && (
+        <label className="lg:p-2 text-white lg:text-xl lg:w-2/5 sm:9-2 sm:text-2xl  ">
+          {label}
+        </label>
+      )}
       <input
         className={`border-2 border-red-400 w-full rounded-md p-2 `}
         onChange={onChange}
