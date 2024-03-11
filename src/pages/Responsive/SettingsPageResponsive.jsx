0cd9f2d5614f3b125fe4396ae93d9c7e6ee00545
Tomas De Piano <tomasdepiano1@gmail.com>
2024-03-10 19:50:18 -0600

    Made some changes to the Settings Page for responsive; still need to work on this page

--- src/pages/Responsive/SettingsPageResponsive.jsx
+++ src/pages/Responsive/SettingsPageResponsive.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Input from "../../components/Input";
 
 function SettingsButton() {
   const user = useSelector((state) => state.user);
+  const email = useSelector((state) => state.email);
+
   return (
     <>
-      <main className="bg-blue-400 h-screen">
-        <div
-          className=" lg:mt-10 lg:ml-2 lg:text-white lg:text-lg lg:w-1/3
-          xxs:mt-10 xxs:ml-28 xxs:text-white"
-        >
+      <main className="xxs:bg-blue-400 xxs:h-screen xxs:flex ">
+        <div className=" xxs:mt-10 xxs:ml-[40%] xxs:justify-center xxs:text-white">
           {" "}
           Hello,{" "}
           <span className="font-bold text-2xl text-red-700">{user} !</span>
+        </div>
+        <div className="fixed top-[35%] ml-[20%] text-white text-bold text-lg">
+          <p>Username: {user}</p>
+        </div>
+        <div className="fixed top-[40%] ml-[20%] text-white text-bold text-lg">
+          <p>Email: {email}</p>
+        </div>
+        <div className="fixed top-[45%] ml-[20%] text-white text-bold text-lg">
+          <p>Password: {<Input />}</p>
         </div>
       </main>
     </>
