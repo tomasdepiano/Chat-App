3b98332d4994d30e0dba8ba1632974d072687c19
Tomas De Piano <tomasdepiano1@gmail.com>
2024-03-11 16:54:04 -0600

    Added some functionality to the responsive settings page, added buttons to the screen and modals pop up when clicking the buttons

--- src/pages/Responsive/SettingsPageResponsive.jsx
+++ src/pages/Responsive/SettingsPageResponsive.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import Input from "../../components/Input";
+import useOpenCloseModal from "../../hooks/useOpenCloseModal";
+import ChangeUserName from "../../modals/ChangeUserName";
+import ChangeEmail from "../../modals/ChangeEmail";
+import ChangePassWord from "../../modals/ChangePassword";
 
 function SettingsButton() {
   const user = useSelector((state) => state.user);
   const email = useSelector((state) => state.email);
+  const [changeUserName, setChangeUserName, closeUserName] =
+    useOpenCloseModal(false);
+  const [changePassWord, setChangePassWord, closePassword] =
+    useOpenCloseModal(false);
+  const [changeEmail, setChangeEmail, closeEmail] = useOpenCloseModal(false);
 
   return (
     <>
@@ -14,15 +22,48 @@ function SettingsButton() {
           Hello,{" "}
           <span className="font-bold text-2xl text-red-700">{user} !</span>
         </div>
-        <div className="fixed top-[35%] ml-[20%] text-white text-bold text-lg">
-          <p>Username: {user}</p>
+        <div className="fixed top-[35%] ml-[20%] text-white text-bold text-lg mt-5">
+          <p>
+            Username: {user}{" "}
+            <button
+              onClick={() => {
+                setChangeUserName(true);
+              }}
+              className="border-2 border-gray-400 rounded-lg ml-2 p-1.5 bg-gray-100 text-black hover:bg-gray-400"
+            >
+              Change Username
+            </button>
+          </p>
         </div>
-        <div className="fixed top-[40%] ml-[20%] text-white text-bold text-lg">
-          <p>Email: {email}</p>
+        <div className="fixed top-[40%] ml-[20%] text-white text-bold text-lg mt-12">
+          <p>
+            Email: {email}{" "}
+            <button
+              onClick={() => {
+                setChangeEmail(true);
+              }}
+              className="border-2 border-gray-400 rounded-lg ml-2 p-1.5 bg-gray-100 text-black hover:bg-gray-400"
+            >
+              Change Email
+            </button>
+          </p>
         </div>
-        <div className="fixed top-[45%] ml-[20%] text-white text-bold text-lg">
-          <p>Password: {<Input />}</p>
+        <div className="fixed top-[45%] ml-[20%] text-white text-bold text-lg mt-20">
+          <p>
+            Password: ******{" "}
+            <button
+              onClick={() => {
+                setChangePassWord(true);
+              }}
+              className="border-2 border-gray-400 rounded-lg ml-2 p-1.5 bg-gray-100 text-black hover:bg-gray-400"
+            >
+              Change Password
+            </button>
+          </p>
         </div>
+        <ChangeUserName onClose={closeUserName} visible={changeUserName} />
+        <ChangePassWord onClose={closePassword} visible={changePassWord} />
+        <ChangeEmail onClose={closeEmail} visible={changeEmail} />
       </main>
     </>
   );
