60bbea34252f11bd528d6db99ccf498e00875217
Mounika Krishna <mounikaa.krishna@gmail.com>
2024-03-12 12:09:12 -0600

    worked on createMessageAPI tested using postman need to work on sending message

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,12 +5,14 @@ import { Router } from "express";
 import createAcountRoutes from "./createAccount.routes.js";
 import authRoutes from "./auth.routes.js";
 import chats from "./createChat.routes.js";
+import createMessageRouter from "./createMessage.routes.js";
 
 const router = Router();
 
 router.use("/api", createAcountRoutes);
 router.use("/api", authRoutes);
 router.use('/api', chats);
+router.use('/api', createMessageRouter)
 
 
 export default router;
