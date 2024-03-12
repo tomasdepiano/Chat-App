2933dbfa5e7c7b10114f99a50e68124e66aebe1f
Mounika Krishna <mounikaa.krishna@gmail.com>
2024-03-12 16:47:58 -0600

    worked on gettingMessages Api need to check with the route with postman

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,12 +5,14 @@ import { Router } from "express";
 import createAcountRoutes from "./createAccount.routes.js";
 import authRoutes from "./auth.routes.js";
 import chats from "./createChat.routes.js";
+import getMessages from "./getMessages.routes.js";
 
 const router = Router();
 
 router.use("/api", createAcountRoutes);
 router.use("/api", authRoutes);
 router.use('/api', chats);
+router.use('/api', getMessages)
 
 
 export default router;
