7d558248abfce5fc8632a96dd32c4e12f45fb947
Tomas De Piano <tomasdepiano1@gmail.com>
2024-03-14 14:15:04 -0600

    Added the useNavigate import to both the settings and logout components; both buttons are now working properly on the site

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
+  const navigate = useNavigate();
   const handleLogout = () => {
-    navigate('/');
+    navigate("/");
   };
   return (
     <div>
