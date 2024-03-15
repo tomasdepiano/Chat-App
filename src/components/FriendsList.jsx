253d516d49b6dafc1f5b89ffb4f2c2202eeddea5
Tomas De Piano <tomasdepiano1@gmail.com>
2024-03-15 14:44:29 -0600

    Made the newMessage Modal looks a little better; made it into a smaller box and added an X button, search bar and compose message icon; still need to do a lot more work on this

--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import SettingsIcon from '@mui/icons-material/Settings';
-import LogoutIcon from '@mui/icons-material/Logout';
-import Logout from '../components/Logout.jsx';
-import Settings from '../components/Settings.jsx';
-import { useNavigate } from 'react-router-dom';
-
-import GroupAddOutlinedIcon from '@mui/icons-material/GroupAddOutlined';
+import React from "react";
+import SettingsIcon from "@mui/icons-material/Settings";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Logout from "../components/Logout.jsx";
+import Settings from "../components/Settings.jsx";
+import { useNavigate } from "react-router-dom";
+import GroupAddOutlinedIcon from "@mui/icons-material/GroupAddOutlined";
 
 const FriendsList = () => {
   const navigate = useNavigate();
 
   function SettingsPageResponsive() {
-    navigate('/settings2');
+    navigate("/settings2");
   }
 
   function ResponsiveLogoutButton() {
-    navigate('/');
+    navigate("/");
   }
 
   return (
     // division for friendsList and buttons(settings&logout)
     <div className="w-1/4 flex-none  ">
       <div className="lg:mt-10 lg:ml-10 text-white lg:text-xl lg:w-[80%] lg:flex lg:justify-between  xxs:hidden ">
-        <p>Friends List</p>{' '}
+        <p>Friends List</p>{" "}
         <div className="border-2 border-red-400 rounded-lg hover:bg-gray-400 text-black bg-gray-100">
           <button>Add Friend</button>
         </div>
