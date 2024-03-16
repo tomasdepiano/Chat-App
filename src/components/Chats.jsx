a03d67db4524b34523630392e0d3d4ba88f7797f
Tomas De Piano <tomasdepiano1@gmail.com>
2024-03-16 14:19:30 -0600

    Made a new route to get all the users from the DB; used that endpoint to display that data on the Chats.jsx file

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import NewMessageModal from '../modals/NewMessageModal.jsx';
-import useOpenCloseModal from '../hooks/useOpenCloseModal';
-
-import MapsUgcOutlinedIcon from '@mui/icons-material/MapsUgcOutlined';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import NewMessageModal from "../modals/NewMessageModal.jsx";
+import useOpenCloseModal from "../hooks/useOpenCloseModal";
+import MapsUgcOutlinedIcon from "@mui/icons-material/MapsUgcOutlined";
+import axios from "axios";
 const Chats = () => {
   const user = useSelector((state) => state.user);
   const [showModal, setShowModal, closeModal] = useOpenCloseModal(false);
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/allUsers").then((res) => {
+      console.log(res);
+      setUserList(res.data);
+    });
+  }, []);
+
   return (
-    <div className=" lg:mt-10 lg:ml-2 lg:text-white lg:text-lg lg:w-1/3 xxs:mt-10 xxs:ml-28 xxs:text-white">
+    <main className=" lg:mt-10 lg:ml-2 lg:text-white lg:text-lg lg:w-1/3 xxs:mt-10 xxs:ml-28 xxs:text-white">
       Hello, <span className="font-bold text-2xl text-red-700">{user} !</span>
       <button
         className="ml-2"
@@ -20,7 +29,16 @@ const Chats = () => {
         <MapsUgcOutlinedIcon fontSize="large" />
       </button>
       <NewMessageModal onClose={closeModal} visible={showModal} />
-    </div>
+      <div className="text-white p-10">
+        {userList.map((person) => {
+          return (
+            <div className="p-10">
+              {person.fname} {person.lname} {`(${person.username})`}
+            </div>
+          );
+        })}
+      </div>
+    </main>
   );
 };
 
