e4fe7a795b62d5cab1d787c58518ce11865d3e9a
Mounika Krishna <mounikaa.krishna@gmail.com>
2024-03-19 17:26:28 -0600

    worked on create message api

--- server/routes/createMessage.routes.js
+++ server/routes/createMessage.routes.js
@@ -6,31 +6,24 @@ const createMessageRouter = Router();
 //grabbing data from messages model
 createMessageRouter.post('/message', async (req, res) => {
   try {
-    const { ChatId, senderId, text, receiverId } = req.body;
+    const { ChatId, senderId, text, receiverId = '' } = req.body;
     if (!senderId || !text)
       return res.status(400).json({ message: "Please fill all required fields" }) // need to fill both senderId and message text
 
-    let chat;
+
     //if chatId is not provided, create a new Chat
     if (!ChatId && receiverId) {
-      chat = new Chats({  // we can also declare const chat= newChats 
+      const chat = new Chats({
         $or: [{ senderId: userId }, { receiverId: userId }]
       });
       await chat.save();
 
     } else if (!ChatId && !receiverId) {
       return res.status(400).send('Please provide reciever Id to start a new chat.');
-    } else {
-      //if chatId is provided, find the existing Chat
-      chat = await Chats.findById(ChatId);
-      if (!chat) {
-        return res.status(404).json({ message: "Chat not found." });
-
-      }
     }
-    const newMessage = new Messages({ ChatId: chat._id, senderId, text })
+    const newMessage = new Messages({ ChatId, senderId, text })
     await newMessage.save();
-    return res.status(200).json(newMessage);
+    res.status(200).json(newMessage);
 
   } catch (error) {
     console.log();
