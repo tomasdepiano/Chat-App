f6944727a1bbb34aec813ae196e088ee9c2d8aad
Mounika Krishna <mounikaa.krishna@gmail.com>
2024-03-19 20:55:17 -0600

    get message API is working successfully. Tested with postman the route is working.

--- server/routes/getMessages.routes.js
+++ server/routes/getMessages.routes.js
@@ -1,10 +1,6 @@
 import { Router } from "express";
-// import Chats from "../models/Chats.model.js";
-
-import { User, Chats, Messages } from "../models/index.js";
-
+import { User, Messages } from "../models/index.js";
 const getMessages = Router();
-
 
 //getting messages
 
@@ -14,12 +10,6 @@ getMessages.get('/messages/:chatId', async (req, res) => {
     if (chatId === "new") return res.status(200).json([]); // if there is no conversation Id it will return Empty array. 
     // if conversation id is already exist it continue to find that conversation Id and start sending messages
     const messages = await Messages.findAll({ include: User, where: { chatId: chatId } });
-    // const messageUserData = Promise.all(messages.map(async (message) => {
-    //   const user = await User.findByPk(message.senderId);
-    //   return {
-    //     user: { email: user.email, fullName: user.fullName }, message: message.message
-    //   }
-    // }));
     res.status(200).json(messages)
   } catch (error) {
     console.log("Error:", error);
